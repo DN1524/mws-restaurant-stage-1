01832487bb3d7c7f1bde9165045052770d1bafdf
Daniel Martinez <DN4251@Gmail.com>
2018-09-10 14:25:02 -0500

    Add alt tags to restaurant images

--- js/main.js
+++ js/main.js
@@ -161,6 +161,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute("alt", restaurant.altTag);
+  image.setAttribute("role", "img");
   li.append(image);
 
   const name = document.createElement('h1');
