a8fba3f8d6900b49f272d2ae2f274be591a5034a
Daniel Martinez <DN4251@Gmail.com>
2018-09-13 17:57:26 -0500

    Add file to check Service Worker compatibility

---
+++ js/check_sw.js
@@ -0,0 +1,9 @@
+// Makes sure that service workers are supported
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker
+			.register("/sw_cache.js")
+			.then(reg => console.log("Service Worker: Registered!"))
+			.catch(err => console.log(`Service Worker: Error: ${err}`));
+	})
+}
